fa16512f20dd460b221e44cfb8728a00f71ca46d
Asus <farashp23@gmail.com>
2020-07-11 18:53:51 +0700

    first

---
+++ src/components/Article.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./article.css";
+const Article = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <h2>Polisi Sebut Anjing K9 Kurang Maksimal Lacak Kasus Tewasnya Editor Metro TV</h2>
+        <img
+          src="https://akcdn.detik.net.id/community/media/visual/2020/07/11/pelacakan-anjing-k9_169.jpeg?w=700&q=90"
+          alt="reporter"
+        />
+        <p>
+          Jakarta - Seekor anjing K9 dikerahkan untuk melacak jejak pelaku
+          terkait tewasnya editor Metro TV, Yodi Prabowo, di Ulujami,
+          Pesanggrahan, Jaksel. Polisi menyebut penelusuran anjing pelacak
+          kurang maksimal lantaran jenazah sudah lebih dari 24 jam. "Tetap ada
+          kelemahan menggunakan K-9, yaitu efektifnya menggunakan K-9 itu 8 jam.
+          Sedangkan ini TKP atau kejadian sudah lebih dari 24 jam. Jadi baunya
+          sudah tipis," kata anggota Tim K-9 Ditpolsatwa Baharkam Korsabhara
+          Mabes Polri, Bripka Sugianto, di pinggir Jalan Tol JORR, Ulujami,
+          Pesanggrahan, Jaksel, Sabtu (11/7/2020). Sugianto mengatakan anjing
+          pelacak bekerja efektifnya 8 jam setelah kejadian di TKP. Anjing
+          pelacak masih dapat bekerja maksimal, namun TKP harus dalam kondisi
+          murni. "Jadi efektifnya menggunakan anjing pelacak kriminal atau
+          pelacak umum itu 8 jam. Kalau sudah lebih 8 jam kalau TKP masih murni
+          masih bisa dilacak dengan anjing," jelas Sugianto. "Jadi memang baunya
+          sudah tipis, tapi anjing kita upaya dan anjing walaupun kurang
+          maksimal, tapi hasilnya ya seperti rekan-rekan lihat sendiri,"
+          tambahnya.
+        </p>
+      </div>
+      <div className="container">
+        <h2>Editor Metro TV Tewas, Pacar Cerita Sedih Rencana Nikah di 2022 Kandas</h2>
+      <img
+          src="https://akcdn.detik.net.id/community/media/visual/2020/07/11/pacar-yodi-prabowo-suci-fitri_169.jpeg?w=700&q=90"
+          alt="reporter-nikah"
+        />
+        <p>
+          Jakarta - Kematian editor Metro TV Yodi Prabowo yang diduga dibunuh
+          menyisakan duka bagi keluarga, kerabat dan rekan kerja. Tak terkecuali
+          dengan Suci Fitri, wanita yang telah berpacaran dengan almarhum selama
+          7 tahun itu mengaku sangat kehilangan. "(Pacaran dengan Yodi) 7
+          tahun," ujar Suci saat ditemui usai pemakaman Yodi di TPU Sandratex,
+          Rempoa, Ciputat Timur, Tangerang Selatan, Sabtu (11/7/2020). Suci
+          mengatakan, dirinya dan almarhum memiliki rencana untuk menikah.
+          Rencana mereka berdua, hari bahagia itu diselenggarakan pada 2022
+          mendatang.Sebelumnya, Suci Fitri, pacar editor Metro TV Yodi Prabowo,
+          mengungkap ada gelagat aneh sebelum Yodi ditemukan tewas. Menurutnya,
+          almarhum bercerita kepada temannya bahwa ia merasa ada hawa mistis di
+          dalam dirinya. "Dengar-dengar sih, dia lagi ngerasain hal-hal yang
+          nggak enak. Kayak hal mistis. Jadi dia itu lagi sensitif banget
+          sama... ya merasanya ada sesuatu," ujar Suci saat ditemui seusai
+          pemakaman di TPU Sandratex, Rempoa, Ciputat Timur, Tangerang Selatan,
+          Sabtu (11/7). Suci mencontohkan, almarhum sempat merasakan hawa yang
+          tidak baik ketika melakukan pemotretan untuk temannya yang sedang
+          prewedding. Lokasi pemotretan berada di Puncak, Bogor, Jawa Barat.
+          "Karena sempat awal bulan lalu dia kan fotoin temannya lagi prewed di
+          Puncak, katanya 'jangan di situ, hawanya nggak enak'," ucapnya.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import './Layout.css';
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Navigation />
+   
+
+      <div className="Layout">
+        <h1>Welcome </h1>
+      </div>
+    
+    </React.Fragment>
+  );
+};
+
+export default Home;
