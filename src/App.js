a62795b11fa493539461b9431bdaf516c87b57a8
Asus <farashp23@gmail.com>
2020-07-12 10:48:09 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Articles from './pages/Articles';
 import About from './pages/About';
 
+function Title(props)
+{
+  return(
+    <div className="judul">{props.title}</div>
+  )
+}
+
+
 function App() {
   return (
     <Router>
       <div className="app-header">
+        <Title title="Selamat datang !!!!" />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/articles" component={Articles} />
